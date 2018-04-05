3833299fb344a03618c6daabb4d93057ff0cba7f
DESKTOP-KD3BN92\57224 <13228803516@163.com>
2018-04-05 17:08:17 +0800

    quartz定时任务

---
+++ sever-quartz/src/test/java/severquartz/severquartz/SeverQuartzApplicationTests.java
@@ -0,0 +1,16 @@
+package severquartz.severquartz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeverQuartzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
