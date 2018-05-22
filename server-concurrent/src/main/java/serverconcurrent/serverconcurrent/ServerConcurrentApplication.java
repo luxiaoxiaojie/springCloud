f7ba248601f21a95359684c6b71bc3383247dabd
DESKTOP-KD3BN92\57224 <13228803516@163.com>
2018-05-22 13:47:21 +0800

    并发包

---
+++ server-concurrent/src/main/java/serverconcurrent/serverconcurrent/ServerConcurrentApplication.java
@@ -0,0 +1,12 @@
+package serverconcurrent.serverconcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerConcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerConcurrentApplication.class, args);
+	}
+}
